87ff4ccd15a8d9a9b5b41fe5f4c7ba5807e3ab4f
CristinaCG <cristinacg1993@gmail.com>
2024-05-04 12:03:35 +0200

    create catalog app

--- my_library/urls.py
+++ my_library/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += [
+    path('book_catalog/', include('book_catalog.urls')),
+    path('', RedirectView.as_view(url='/book_catalog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
