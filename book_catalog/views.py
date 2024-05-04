9ecc259ba95e96d9d1ff5974b13a2a8e772ad18f
CristinaCG <cristinacg1993@gmail.com>
2024-05-04 17:39:21 +0200

    list_view for book and author

--- book_catalog/views.py
+++ book_catalog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Author, Book, Genre, Language, BookSaga
+from django.views import generic
 
-# Create your views here.
+def index(request):
+    """
+    View function for home page of site.
+    """
+    num_books=Book.objects.all().count()
+    num_authors=Author.objects.count()
+    return render(
+        request,
+        'index_book.html',
+        context = {'num_books':num_books, 'num_authors':num_authors},
+    )
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+# class BookDetailView(generic.DetailView):
+#     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+# class AuthorDetailView(generic.DetailView):
+    # model = Author
