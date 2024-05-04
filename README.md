3b0caee2a58306fa0c8cc38147a2fb7f142f05be
CristinaCG <cristinacg1993@gmail.com>
2024-05-04 11:45:16 +0200

    start over

--- README.md
+++
@@ -1,1 +0,0 @@
-# tokio_project
--- person_catalog/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PersonCatalogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'person_catalog'
